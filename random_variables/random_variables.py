c218a584f1ccffcd71e10c17dfd0f4411455e2db
Alex Santoyo <ale.santoyo.c@gmail.com>
2025-05-05 13:56:27 +0100

    Including a pyhton notebook to test changes

--- random_variables/random_variables.py
+++ random_variables/random_variables.py
@@ -4,24 +4,52 @@
 # imports
 import numpy as np
 from scipy.stats import binom
+# from IPython.display import display, Math, Latex
 
 # this will be the parent class
 class RandomVariable:
-    def __init__(self):
-        pass
+    """
+    General class for a Random Variable
+    """
+    def __init__(self, rv_name = None, **params):
+        self.rv_name = rv_name
+        self.params = params
+
+    def __str__(self):
+        return(f"A {self.rv_name} random variable instance.")
 
 # let's try a few simple examples first, to see how to better define the parent class...
 
-class BinomialRV:
-    def __init__(self, n, p):
+class BinomialRV(RandomVariable):
+    """
+    Class of a Binomial Random Variable.
+    """
+    def __init__(self, n = 1, p = 0.5):
+        """
+        Parameters:
+        n, number of independent Bernoulli experiments
+        p, success probability on each Bernoulli experiment
+        """
+        self.rv_name = 'Binomial'
         self.n = n
         self.p = p
         self.q = 1 - p
 
+        super().__init__(rv_name=self.rv_name, n = 1, p = 0.5)
+
     def expectation(self):
+        """
+        E(X) = n*p
+        """
+        # Latex(r'$\mu = \frac{1}{n} \sum_{i=1}^n x_i$')
+        # display(Math(r'F(k) = \int_{-\infty}^{\infty} f(x) e^{2\pi i k} dx'))
+
         return self.n * self.p
     
     def variance(self):
+        """
+        Var(X) = n*p*q
+        """
         return self.n * self.p * self.q
     
     def mgf(self):
